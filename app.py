cda0f5ee217e85eb585f984418f04527df77bfe3
cyber <bps7965@gmail.com>
2025-04-12 10:47:58 +0900

    데이터베이스 연동완료

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 socketio = SocketIO(app)
 
-udatabase=create_engine(app.config['UDB_URL'],pool_pre_ping=True ,pool_recycle=3600,echo=True)
-kdatabase=create_engine(app.config['KDB_URL'],pool_pre_ping=True,pool_recycle=3600,echo=True)
-cdatabase=create_engine(app.config['CDB_URL'],pool_pre_ping=True,pool_recycle=3600,echo=True)
+database=create_engine(app.config['DB_URL'],pool_pre_ping=True ,pool_recycle=3600,echo=True)
+
 
 
 def encrypt_message(message, public_key):
@@ -34,7 +33,7 @@ def get_login(user_id):
     query = text("SELECT id, passwd FROM PRIVATE WHERE id = :id") 
     #:id는 플레이스 홀더역할로 값을 넣을 자리를 표시하는 역할을 한단다 이후.execute({"id":user_id})에서 user_id 	값을 전달하면 실행시,:id가 해당값으로 대체된다고함
 
-    with kdatabase.connect() as conn:
+    with database.connect() as conn:
         result = conn.execute(query, {"id": user_id}).fetchone()
 	#user_id="u001"로 입력했다 치면 실행시 :id가 "u001"로 대체된다
 	#SQLAlchemy가 '--같은것도 그냥 문자열로만 취급하기때문에 인젝션이 막힌다함
@@ -44,7 +43,7 @@ def get_login(user_id):
         return
 def get_duplication(user_id):
     query = text("SELECT id FROM USERINFO WHERE id = :id")
-    with udatabase.connect() as conn:
+    with database.connect() as conn:
         result = conn.execute(query, {"id": user_id}).fetchone()
         if result:
             return  {"id":result[0]}
@@ -63,35 +62,63 @@ def sign_in():
     userinfo=get_login(user_id)
     if userinfo:
         if userinfo["passwd"]==password:
-            print("쿠키 설정")#변경할것
+            return "쿠키 설정"#변경할것
         else:
-            print("아이디 혹은 비밀번호가 다릅니다.")
+            return "아이디 혹은 비밀번호가 다릅니다."
 
     else:
-        print("아이디 혹은 비밀번호가 다릅니다.")
-@app.route('/register',methods=['POST'])
+        return "아이디 혹은 비밀번호가 다릅니다."
+
+@app.route('/register',methods=['GET','POST'])
 def sign_up():
-    user_name=request.form.get("user_name")
-    user_id=request.form.get("user_id")
-    password=request.form.get("password")
-    duplication=get_duplication(user_id)
-    if duplication:
-        print("사용할 수 없는 아이디입니다.")
-               
-    else:
+    if request.method == 'POST':
+        user_name=request.form.get("user_name")
+        user_id=request.form.get("user_id")
+        password=request.form.get("password")
+        duplication=get_duplication(user_id)
+        if duplication:
+            return "사용할 수 없는 아이디입니다."
+                    
         private_key, public_key = make_key()
+        pri_str = private_key.export_key().decode()
+        pub_str = public_key.export_key().decode()
 
+        try:
+            uquery=text("""INSERT INTO USERINFO(name,id,pub_key) VALUES (:name,:id,:pub_key)""")
+            with database.connect() as conn:
+                conn.execute(uquery,{"name":user_name,"id":user_id,"pub_key":pub_str})
+                conn.commit()
+            kquery=text("""INSERT INTO PRIVATE(id,passwd,pri_key) VALUES (:id,:passwd,:pri_key)""")
+            with database.connect() as conn:
+                conn.execute(kquery,{"id":user_id,"passwd":password,"pri_key":pri_str})
+                conn.commit()
 
+            return "회원가입 성공" 
+        
+        except Exception as e:
+            print("에러 발생:", e)
+            return "회원가입 실패"
+    return render_template('register.html')
 
 @app.route('/openchat')        
 def chat():
-    return render_template('webchat.html')  # HTML 파일 렌더링
+    query=text("""SELECT chat FROM ACHAT ORDER BY num ASC""")
+    with database.connect() as conn:
+        result=conn.execute(query).fetchall()
+        chats=[{"chat":row[0]} for row in result]
+    return render_template('webchat.html',chats=chats)  # HTML 파일 렌더링
  
 @socketio.on('message')
 def handle_message(msg):
-    print(f"Received message: {msg}")
-    send(msg, broadcast=True)  # 모든 클라이언트에게 메시지
-
+      
+    user_ip=request.remote_addr
+    print(f"[{user_ip}] 메세지 수신:{msg}")
+    query=text("""INSERT INTO ACHAT(chat) VALUES(:chat)""")
+    with database.connect() as conn:
+        conn.execute(query,{"chat":msg})
+        conn.commit()
+    send(msg, broadcast=True)# 모든 클라이언트에게 메시지
+    
     
 
 
@@ -104,7 +131,7 @@ def handle_message(msg):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
 #if __name__ == '__main__':
 #    app.run(debug=True)
--- config.py
+++ config.py
@@ -1,31 +1,16 @@
 dbs = {
-    'userdb': {
-        'host': 'ip주소',
-        'user': '계정id',
-        'password': '계정비밀번호',
-        'database': 'USERINFO',
+    'host': '34.64.125.44',
+        'user': 'recong',
+        'password': 'recong',
+        'database': 'project',
         'port': '3306'
-    },
-    'keydb': {
-        'host': 'ip주소',
-        'user': '계정id',
-        'password': '계정비밀번호',
-        'database': 'PRIVATE',
-        'port': '3306'
-    },
-    'chatdb':{
- 	'host': 'ip주소',
-        'user': '계정id',
-        'password': '계정비밀번호',
-        'database': 'CHAT',
-        'port': '3306'
-}
+
 }
 
 
-UDB_URL = f"mysql+pymysql://{dbs['userdb']['user']}:{dbs['userdb']['password']}@{dbs['userdb']['host']}:{dbs['userdb']['port']}/{dbs['userdb']['database']}?charset=utf8mb4"
-KDB_URL = f"mysql+pymysql://{dbs['keydb']['user']}:{dbs['keydb']['password']}@{dbs['keydb']['host']}:{dbs['keydb']['port']}/{dbs['keydb']['database']}?charset=utf8mb4"
-CDB_URL = f"mysql+pymysql://{dbs['chatdb']['user']}:{dbs['chatdb']['password']}@{dbs['chatdb']['host']}:{dbs['chatdb']['port']}/{dbs['chatdb']['database']}?charset=utf8mb4"
+DB_URL = f"mysql+pymysql://{dbs['user']}:{dbs['password']}@{dbs['host']}:{dbs['port']}/{dbs['database']}?charset=utf8mb4"
+
 #SQLAlchemy에서 MySQL에 연결할 때 사용되는 URL을 생성
 
 #데이터베이스 설정을 따로 관리하는 파일 config.py
+#sudo iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
